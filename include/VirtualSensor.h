2cb2ab3ebd070b84a7b90f014c6e92418d59e1f2
Emil <emil@esnx.xyz>
2024-01-06 17:27:54 +0400

    Run formatter

--- include/VirtualSensor.h
+++ include/VirtualSensor.h
@@ -1,274 +1,244 @@
 #pragma once
 
-#include <vector>
-#include <iostream>
 #include <cstring>
 #include <fstream>
+#include <iostream>
+#include <vector>
 
 #include "Eigen.h"
 #include "FreeImageHelper.h"
 
 typedef unsigned char BYTE;
 
-// reads sensor files according to https://vision.in.tum.de/data/datasets/rgbd-dataset/file_formats
-class VirtualSensor
-{
-public:
+// reads sensor files according to
+// https://vision.in.tum.de/data/datasets/rgbd-dataset/file_formats
+class VirtualSensor {
+   public:
+    VirtualSensor() : m_currentIdx(-1), m_increment(10) {}
 
-	VirtualSensor() : m_currentIdx(-1), m_increment(10)
-	{
+    ~VirtualSensor() {
+        SAFE_DELETE_ARRAY(m_depthFrame);
+        SAFE_DELETE_ARRAY(m_colorFrame);
+    }
 
-	}
+    bool Init(const std::string& datasetDir) {
+        m_baseDir = datasetDir;
 
-	~VirtualSensor()
-	{
-		SAFE_DELETE_ARRAY(m_depthFrame);
-		SAFE_DELETE_ARRAY(m_colorFrame);
-	}
+        // read filename lists
+        if (!ReadFileList(datasetDir + "depth.txt", m_filenameDepthImages,
+                          m_depthImagesTimeStamps))
+            return false;
+        if (!ReadFileList(datasetDir + "rgb.txt", m_filenameColorImages,
+                          m_colorImagesTimeStamps))
+            return false;
 
-	bool Init(const std::string& datasetDir)
-	{
-		m_baseDir = datasetDir;
+        // read tracking
+        if (!ReadTrajectoryFile(datasetDir + "groundtruth.txt", m_trajectory,
+                                m_trajectoryTimeStamps))
+            return false;
 
-		// read filename lists
-		if (!ReadFileList(datasetDir + "depth.txt", m_filenameDepthImages, m_depthImagesTimeStamps)) return false;
-		if (!ReadFileList(datasetDir + "rgb.txt", m_filenameColorImages, m_colorImagesTimeStamps)) return false;
+        if (m_filenameDepthImages.size() != m_filenameColorImages.size())
+            return false;
 
-		// read tracking
-		if (!ReadTrajectoryFile(datasetDir + "groundtruth.txt", m_trajectory, m_trajectoryTimeStamps)) return false;
+        // image resolutions
+        m_colorImageWidth = 640;
+        m_colorImageHeight = 480;
+        m_depthImageWidth = 640;
+        m_depthImageHeight = 480;
 
-		if (m_filenameDepthImages.size() != m_filenameColorImages.size()) return false;
+        // intrinsics
+        m_colorIntrinsics << 525.0f, 0.0f, 319.5f, 0.0f, 525.0f, 239.5f, 0.0f,
+            0.0f, 1.0f;
 
-		// image resolutions
-		m_colorImageWidth = 640;
-		m_colorImageHeight = 480;
-		m_depthImageWidth = 640;
-		m_depthImageHeight = 480;
+        m_depthIntrinsics = m_colorIntrinsics;
 
-		// intrinsics
-		m_colorIntrinsics <<	525.0f, 0.0f, 319.5f,
-								0.0f, 525.0f, 239.5f,
-								0.0f, 0.0f, 1.0f;
+        m_colorExtrinsics.setIdentity();
+        m_depthExtrinsics.setIdentity();
 
-		m_depthIntrinsics = m_colorIntrinsics;
+        m_depthFrame = new float[m_depthImageWidth * m_depthImageHeight];
+        for (unsigned int i = 0; i < m_depthImageWidth * m_depthImageHeight;
+             ++i)
+            m_depthFrame[i] = 0.5f;
 
-		m_colorExtrinsics.setIdentity();
-		m_depthExtrinsics.setIdentity();
+        m_colorFrame = new BYTE[4 * m_colorImageWidth * m_colorImageHeight];
+        for (unsigned int i = 0; i < 4 * m_colorImageWidth * m_colorImageHeight;
+             ++i)
+            m_colorFrame[i] = 255;
 
-		m_depthFrame = new float[m_depthImageWidth*m_depthImageHeight];
-		for (unsigned int i = 0; i < m_depthImageWidth*m_depthImageHeight; ++i) m_depthFrame[i] = 0.5f;
+        m_currentIdx = -1;
+        return true;
+    }
 
-		m_colorFrame = new BYTE[4* m_colorImageWidth*m_colorImageHeight];
-		for (unsigned int i = 0; i < 4*m_colorImageWidth*m_colorImageHeight; ++i) m_colorFrame[i] = 255;
+    bool ProcessNextFrame() {
+        if (m_currentIdx == -1)
+            m_currentIdx = 0;
+        else
+            m_currentIdx += m_increment;
 
+        if ((unsigned int)m_currentIdx >=
+            (unsigned int)m_filenameColorImages.size())
+            return false;
 
-		m_currentIdx = -1;
-		return true;
-	}
+        std::cout << "ProcessNextFrame [" << m_currentIdx << " | "
+                  << m_filenameColorImages.size() << "]" << std::endl;
 
-	bool ProcessNextFrame()
-	{
-		if (m_currentIdx == -1)	m_currentIdx = 0;
-		else m_currentIdx += m_increment;
+        FreeImageB rgbImage;
+        rgbImage.LoadImageFromFile(m_baseDir +
+                                   m_filenameColorImages[m_currentIdx]);
+        memcpy(m_colorFrame, rgbImage.data, 4 * 640 * 480);
 
-		if ((unsigned int)m_currentIdx >= (unsigned int)m_filenameColorImages.size()) return false;
+        // depth images are scaled by 5000 (see
+        // https://vision.in.tum.de/data/datasets/rgbd-dataset/file_formats)
+        FreeImageU16F dImage;
+        dImage.LoadImageFromFile(m_baseDir +
+                                 m_filenameDepthImages[m_currentIdx]);
 
-		std::cout << "ProcessNextFrame [" << m_currentIdx << " | " << m_filenameColorImages.size() << "]" << std::endl;
+        for (unsigned int i = 0; i < m_depthImageWidth * m_depthImageHeight;
+             ++i) {
+            if (dImage.data[i] == 0)
+                m_depthFrame[i] = MINF;
+            else
+                m_depthFrame[i] = dImage.data[i] * 1.0f / 5000.0f;
+        }
 
-		FreeImageB rgbImage;
-		rgbImage.LoadImageFromFile(m_baseDir + m_filenameColorImages[m_currentIdx]);
-		memcpy(m_colorFrame, rgbImage.data, 4 * 640 * 480);
+        // find transformation (simple nearest neighbor, linear search)
+        double timestamp = m_depthImagesTimeStamps[m_currentIdx];
+        double min = std::numeric_limits<double>::max();
+        int idx = 0;
+        for (unsigned int i = 0; i < m_trajectory.size(); ++i) {
+            double d = fabs(m_trajectoryTimeStamps[i] - timestamp);
+            if (min > d) {
+                min = d;
+                idx = i;
+            }
+        }
+        m_currentTrajectory = m_trajectory[idx];
 
-		// depth images are scaled by 5000 (see https://vision.in.tum.de/data/datasets/rgbd-dataset/file_formats)
-		FreeImageU16F dImage;
-		dImage.LoadImageFromFile(m_baseDir + m_filenameDepthImages[m_currentIdx]);
+        return true;
+    }
 
-		for (unsigned int i = 0; i < m_depthImageWidth*m_depthImageHeight; ++i)
-		{
-			if (dImage.data[i] == 0)
-				m_depthFrame[i] = MINF;
-			else
-				m_depthFrame[i] = dImage.data[i] * 1.0f / 5000.0f;
-		}
+    unsigned int GetCurrentFrameCnt() { return (unsigned int)m_currentIdx; }
 
-		// find transformation (simple nearest neighbor, linear search)
-		double timestamp = m_depthImagesTimeStamps[m_currentIdx];
-		double min = std::numeric_limits<double>::max();
-		int idx = 0;
-		for (unsigned int i = 0; i < m_trajectory.size(); ++i)
-		{
-			double d = fabs(m_trajectoryTimeStamps[i] - timestamp);
-			if (min > d)
-			{
-				min = d;
-				idx = i;
-			}
-		}
-		m_currentTrajectory = m_trajectory[idx];
+    // get current color data
+    BYTE* GetColorRGBX() { return m_colorFrame; }
+    // get current depth data
+    float* GetDepth() { return m_depthFrame; }
 
+    // color camera info
+    Eigen::Matrix3f GetColorIntrinsics() { return m_colorIntrinsics; }
 
-		return true;
-	}
+    Eigen::Matrix4f GetColorExtrinsics() { return m_colorExtrinsics; }
 
-	unsigned int GetCurrentFrameCnt()
-	{
-		return (unsigned int)m_currentIdx;
-	}
+    unsigned int GetColorImageWidth() { return m_colorImageWidth; }
 
-	// get current color data
-	BYTE* GetColorRGBX()
-	{
-		return m_colorFrame;
-	}
-	// get current depth data
-	float* GetDepth()
-	{
-		return m_depthFrame;
-	}
+    unsigned int GetColorImageHeight() { return m_colorImageHeight; }
 
-	// color camera info
-	Eigen::Matrix3f GetColorIntrinsics()
-	{
-		return m_colorIntrinsics;
-	}
+    // depth (ir) camera info
+    Eigen::Matrix3f GetDepthIntrinsics() { return m_depthIntrinsics; }
 
-	Eigen::Matrix4f GetColorExtrinsics()
-	{
-		return m_colorExtrinsics;
-	}
+    Eigen::Matrix4f GetDepthExtrinsics() { return m_depthExtrinsics; }
 
-	unsigned int GetColorImageWidth()
-	{
-		return m_colorImageWidth;
-	}
+    unsigned int GetDepthImageWidth() { return m_colorImageWidth; }
 
-	unsigned int GetColorImageHeight()
-	{
-		return m_colorImageHeight;
-	}
+    unsigned int GetDepthImageHeight() { return m_colorImageHeight; }
 
-	// depth (ir) camera info
-	Eigen::Matrix3f GetDepthIntrinsics()
-	{
-		return m_depthIntrinsics;
-	}
+    // get current trajectory transformation
+    Eigen::Matrix4f GetTrajectory() { return m_currentTrajectory; }
 
-	Eigen::Matrix4f GetDepthExtrinsics()
-	{
-		return m_depthExtrinsics;
-	}
+   private:
+    bool ReadFileList(const std::string& filename,
+                      std::vector<std::string>& result,
+                      std::vector<double>& timestamps) {
+        std::ifstream fileDepthList(filename, std::ios::in);
+        if (!fileDepthList.is_open()) return false;
+        result.clear();
+        timestamps.clear();
+        std::string dump;
+        std::getline(fileDepthList, dump);
+        std::getline(fileDepthList, dump);
+        std::getline(fileDepthList, dump);
+        while (fileDepthList.good()) {
+            double timestamp;
+            fileDepthList >> timestamp;
+            std::string filename;
+            fileDepthList >> filename;
+            if (filename == "") break;
+            timestamps.push_back(timestamp);
+            result.push_back(filename);
+        }
+        fileDepthList.close();
+        return true;
+    }
 
-	unsigned int GetDepthImageWidth()
-	{
-		return m_colorImageWidth;
-	}
+    bool ReadTrajectoryFile(const std::string& filename,
+                            std::vector<Eigen::Matrix4f>& result,
+                            std::vector<double>& timestamps) {
+        std::ifstream file(filename, std::ios::in);
+        if (!file.is_open()) return false;
+        result.clear();
+        std::string dump;
+        std::getline(file, dump);
+        std::getline(file, dump);
+        std::getline(file, dump);
 
-	unsigned int GetDepthImageHeight()
-	{
-		return m_colorImageHeight;
-	}
+        while (file.good()) {
+            double timestamp;
+            file >> timestamp;
+            Eigen::Vector3f translation;
+            file >> translation.x() >> translation.y() >> translation.z();
+            Eigen::Quaternionf rot;
+            file >> rot;
 
-	// get current trajectory transformation
-	Eigen::Matrix4f GetTrajectory()
-	{
-		return m_currentTrajectory;
-	}
+            Eigen::Matrix4f transf;
+            transf.setIdentity();
+            transf.block<3, 3>(0, 0) = rot.toRotationMatrix();
+            transf.block<3, 1>(0, 3) = translation;
 
-private:
+            if (rot.norm() == 0) break;
 
-	bool ReadFileList(const std::string& filename, std::vector<std::string>& result, std::vector<double>& timestamps)
-	{
-		std::ifstream fileDepthList(filename, std::ios::in);
-		if (!fileDepthList.is_open()) return false;
-		result.clear();
-		timestamps.clear();
-		std::string dump;
-		std::getline(fileDepthList, dump);
-		std::getline(fileDepthList, dump);
-		std::getline(fileDepthList, dump);
-		while (fileDepthList.good())
-		{
-			double timestamp;
-			fileDepthList >> timestamp;
-			std::string filename;
-			fileDepthList >> filename;
-			if (filename == "") break;
-			timestamps.push_back(timestamp);
-			result.push_back(filename);
-		}
-		fileDepthList.close();
-		return true;
-	}
+            transf = transf.inverse().eval();
 
-	bool ReadTrajectoryFile(const std::string& filename, std::vector<Eigen::Matrix4f>& result, std::vector<double>& timestamps)
-	{
-		std::ifstream file(filename, std::ios::in);
-		if (!file.is_open()) return false;
-		result.clear();
-		std::string dump;
-		std::getline(file, dump);
-		std::getline(file, dump);
-		std::getline(file, dump);
+            timestamps.push_back(timestamp);
+            result.push_back(transf);
+        }
+        file.close();
+        return true;
+    }
 
-		while (file.good())
-		{
-			double timestamp;
-			file >> timestamp;
-			Eigen::Vector3f translation;
-			file >> translation.x() >> translation.y() >> translation.z();
-			Eigen::Quaternionf rot;
-			file >> rot;
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-			Eigen::Matrix4f transf;
-			transf.setIdentity();
-			transf.block<3, 3>(0, 0) = rot.toRotationMatrix();
-			transf.block<3, 1>(0, 3) = translation;
+    // current frame index
+    int m_currentIdx;
 
-			if (rot.norm() == 0) break;
+    int m_increment;
 
-			transf = transf.inverse().eval();
+    // frame data
+    float* m_depthFrame;
+    BYTE* m_colorFrame;
+    Eigen::Matrix4f m_currentTrajectory;
 
-			timestamps.push_back(timestamp);
-			result.push_back(transf);
-		}
-		file.close();
-		return true;
-	}
+    // color camera info
+    Eigen::Matrix3f m_colorIntrinsics;
+    Eigen::Matrix4f m_colorExtrinsics;
+    unsigned int m_colorImageWidth;
+    unsigned int m_colorImageHeight;
 
-	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    // depth (ir) camera info
+    Eigen::Matrix3f m_depthIntrinsics;
+    Eigen::Matrix4f m_depthExtrinsics;
+    unsigned int m_depthImageWidth;
+    unsigned int m_depthImageHeight;
 
-	// current frame index
-	int m_currentIdx;
+    // base dir
+    std::string m_baseDir;
+    // filenamelist depth
+    std::vector<std::string> m_filenameDepthImages;
+    std::vector<double> m_depthImagesTimeStamps;
+    // filenamelist color
+    std::vector<std::string> m_filenameColorImages;
+    std::vector<double> m_colorImagesTimeStamps;
 
-	int m_increment;
-
-	// frame data
-	float* m_depthFrame;
-	BYTE* m_colorFrame;
-	Eigen::Matrix4f m_currentTrajectory;
-
-	// color camera info
-	Eigen::Matrix3f m_colorIntrinsics;
-	Eigen::Matrix4f m_colorExtrinsics;
-	unsigned int m_colorImageWidth;
-	unsigned int m_colorImageHeight;
-
-	// depth (ir) camera info
-	Eigen::Matrix3f m_depthIntrinsics;
-	Eigen::Matrix4f m_depthExtrinsics;
-	unsigned int m_depthImageWidth;
-	unsigned int m_depthImageHeight;
-
-	// base dir
-	std::string m_baseDir;
-	// filenamelist depth
-	std::vector<std::string> m_filenameDepthImages;
-	std::vector<double> m_depthImagesTimeStamps;
-	// filenamelist color
-	std::vector<std::string> m_filenameColorImages;
-	std::vector<double> m_colorImagesTimeStamps;
-
-	// trajectory
-	std::vector<Eigen::Matrix4f> m_trajectory;
-	std::vector<double> m_trajectoryTimeStamps;
+    // trajectory
+    std::vector<Eigen::Matrix4f> m_trajectory;
+    std::vector<double> m_trajectoryTimeStamps;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <fstream>
 
 #include "Eigen.h"
 #include "VirtualSensor.h"
@@ -13,7 +12,8 @@ struct Vertex {
     Vector4uc color;
 };
 
-using VertexAction = std::function<void(const Vertex &, const Vertex &, const Vertex &, const Vertex &)>;
+using VertexAction = std::function<void(const Vertex &, const Vertex &,
+                                        const Vertex &, const Vertex &)>;
 
 bool areAllEdgesValid(const Vertex *vs[4], float edgeThreshold) {
     for (int i = 0; i < 4; ++i) {
@@ -26,14 +26,15 @@ bool areAllEdgesValid(const Vertex *vs[4], float edgeThreshold) {
     return true;
 }
 
-void
-processVertex(unsigned int idx, unsigned int width, Vertex *vertices, float edgeThreshold, const VertexAction &action) {
+void processVertex(unsigned int idx, unsigned int width, Vertex *vertices,
+                   float edgeThreshold, const VertexAction &action) {
     const Vertex &v0 = vertices[idx];
     const Vertex &v1 = vertices[idx + 1];
     const Vertex &v2 = vertices[idx + width];
     const Vertex &v3 = vertices[idx + width + 1];
 
-    if (v0.position.x() != MINF && v1.position.x() != MINF && v2.position.x() != MINF && v3.position.x() != MINF) {
+    if (v0.position.x() != MINF && v1.position.x() != MINF &&
+        v2.position.x() != MINF && v3.position.x() != MINF) {
         const Vertex *vs[4] = {&v0, &v1, &v2, &v3};
 
         if (areAllEdgesValid(vs, edgeThreshold)) {
@@ -43,25 +44,35 @@ processVertex(unsigned int idx, unsigned int width, Vertex *vertices, float edge
     }
 }
 
-bool WriteMesh(Vertex *vertices, unsigned int width, unsigned int height, const std::string &filename) {
-    float edgeThreshold = 0.01f; // 1cm
+bool WriteMesh(Vertex *vertices, unsigned int width, unsigned int height,
+               const std::string &filename) {
+    float edgeThreshold = 0.01f;  // 1cm
 
-    // TODO 2: use the OFF file format to save the vertices grid (http://www.geomview.org/docs/html/OFF.html)
-    // - have a look at the "off_sample.off" file to see how to store the vertices and triangles
-    // - for debugging we recommend to first only write out the vertices (set the number of faces to zero)
-    // - for simplicity write every vertex to file, even if it is not valid (position.x() == MINF) (note that all vertices in the off file have to be valid, thus, if a point is not valid write out a dummy point like (0,0,0))
-    // - use a simple triangulation exploiting the grid structure (neighboring vertices build a triangle, two triangles per grid cell)
-    // - you can use an arbitrary triangulation of the cells, but make sure that the triangles are consistently oriented
-    // - only write triangles with valid vertices and an edge length smaller then edgeThreshold
+    // TODO 2: use the OFF file format to save the vertices grid
+    // (http://www.geomview.org/docs/html/OFF.html)
+    // - have a look at the "off_sample.off" file to see how to store the
+    // vertices and triangles
+    // - for debugging we recommend to first only write out the vertices (set
+    // the number of faces to zero)
+    // - for simplicity write every vertex to file, even if it is not valid
+    // (position.x() == MINF) (note that all vertices in the off file have to be
+    // valid, thus, if a point is not valid write out a dummy point like
+    // (0,0,0))
+    // - use a simple triangulation exploiting the grid structure (neighboring
+    // vertices build a triangle, two triangles per grid cell)
+    // - you can use an arbitrary triangulation of the cells, but make sure that
+    // the triangles are consistently oriented
+    // - only write triangles with valid vertices and an edge length smaller
+    // then edgeThreshold
 
     // TODO: Get number of vertices
     unsigned int nVertices = width * height;
 
     // TODO: Determine number of valid faces
     unsigned nFaces = 0;
-    VertexAction countFaces = [&nFaces](const Vertex &, const Vertex &, const Vertex &, const Vertex &) {
-        nFaces += 2;
-    };
+    VertexAction countFaces = [&nFaces](const Vertex &, const Vertex &,
+                                        const Vertex &,
+                                        const Vertex &) { nFaces += 2; };
     for (unsigned int y = 0; y < height - 1; ++y) {
         for (unsigned int x = 0; x < width - 1; ++x) {
             unsigned int idx = y * width + x;
@@ -82,18 +93,18 @@ bool WriteMesh(Vertex *vertices, unsigned int width, unsigned int height, const 
 
     // Example off file:
     // COFF
-    //# numVertices numFaces numEdges
-    //4 2 0
-    //# list of vertices
-    //# X Y Z R G B A
-    //0.0 1.0 0.0 255 255 255 255
-    //0.0 0.0 0.0 255 255 255 255
-    //1.0 0.0 0.0 255 255 255 255
-    //1.0 1.0 0.0 255 255 255 255
-    //# list of faces
-    //# nVerticesPerFace idx0 idx1 idx2 ...
-    //3 0 1 2
-    //3 0 2 3
+    // # numVertices numFaces numEdges
+    // 4 2 0
+    // # list of vertices
+    // # X Y Z R G B A
+    // 0.0 1.0 0.0 255 255 255 255
+    // 0.0 0.0 0.0 255 255 255 255
+    // 1.0 0.0 0.0 255 255 255 255
+    // 1.0 1.0 0.0 255 255 255 255
+    // # list of faces
+    // # nVerticesPerFace idx0 idx1 idx2 ...
+    // 3 0 1 2
+    // 3 0 2 3
 
     // TODO: save vertices
     outFile << "# list of vertices" << std::endl;
@@ -103,9 +114,10 @@ bool WriteMesh(Vertex *vertices, unsigned int width, unsigned int height, const 
             unsigned int idx = y * width + x;
             Vertex v = vertices[idx];
             if (v.position.x() != MINF) {
-                outFile << v.position.x() << " " << v.position.y() << " " << v.position.z() << " " << (int) v.color.x()
-                        << " " << (int) v.color.y() << " " << (int) v.color.z() << " " << (int) v.color.w()
-                        << std::endl;
+                outFile << v.position.x() << " " << v.position.y() << " "
+                        << v.position.z() << " " << (int)v.color.x() << " "
+                        << (int)v.color.y() << " " << (int)v.color.z() << " "
+                        << (int)v.color.w() << std::endl;
             } else {
                 outFile << "0.0 0.0 0.0 0 0 0 0" << std::endl;
             }
@@ -116,8 +128,9 @@ bool WriteMesh(Vertex *vertices, unsigned int width, unsigned int height, const 
     std::cout << "# list of faces" << std::endl;
     std::cout << "# nVerticesPerFace idx0 idx1 idx2 ..." << std::endl;
 
-    VertexAction writeFaces = [&outFile, &vertices](const Vertex &v0, const Vertex &v1, const Vertex &v2,
-                                                    const Vertex &v3) {
+    VertexAction writeFaces = [&outFile, &vertices](
+                                  const Vertex &v0, const Vertex &v1,
+                                  const Vertex &v2, const Vertex &v3) {
         unsigned int idx0 = &v0 - &vertices[0];
         unsigned int idx1 = &v1 - &vertices[0];
         unsigned int idx2 = &v2 - &vertices[0];
@@ -148,17 +161,20 @@ int main() {
     std::cout << "Initialize virtual sensor..." << std::endl;
     VirtualSensor sensor;
     if (!sensor.Init(filenameIn)) {
-        std::cout << "Failed to initialize the sensor!\nCheck file path!" << std::endl;
+        std::cout << "Failed to initialize the sensor!\nCheck file path!"
+                  << std::endl;
         return -1;
     }
 
     // convert video to meshes
     while (sensor.ProcessNextFrame()) {
         // get ptr to the current depth frame
-        // depth is stored in row major (get dimensions via sensor.GetDepthImageWidth() / GetDepthImageHeight())
+        // depth is stored in row major (get dimensions via
+        // sensor.GetDepthImageWidth() / GetDepthImageHeight())
         float *depthMap = sensor.GetDepth();
         // get ptr to the current color frame
-        // color is stored as RGBX in row major (4 byte values per pixel, get dimensions via sensor.GetColorImageWidth() / GetColorImageHeight())
+        // color is stored as RGBX in row major (4 byte values per pixel, get
+        // dimensions via sensor.GetColorImageWidth() / GetColorImageHeight())
         BYTE *colorMap = sensor.GetColorRGBX();
 
         // get depth intrinsics
@@ -178,11 +194,13 @@ int main() {
 
         // TODO 1: back-projection
         // write result to the vertices array below, keep pixel ordering!
-        // if the depth value at idx is invalid (MINF) write the following values to the vertices array
-        // vertices[idx].position = Vector4f(MINF, MINF, MINF, MINF);
-        // vertices[idx].color = Vector4uc(0,0,0,0);
-        // otherwise apply back-projection and transform the vertex to world space, use the corresponding color from the colormap
-        Vertex *vertices = new Vertex[sensor.GetDepthImageWidth() * sensor.GetDepthImageHeight()];
+        // if the depth value at idx is invalid (MINF) write the following
+        // values to the vertices array vertices[idx].position = Vector4f(MINF,
+        // MINF, MINF, MINF); vertices[idx].color = Vector4uc(0,0,0,0);
+        // otherwise apply back-projection and transform the vertex to world
+        // space, use the corresponding color from the colormap
+        Vertex *vertices = new Vertex[sensor.GetDepthImageWidth() *
+                                      sensor.GetDepthImageHeight()];
         for (unsigned int y = 0; y < sensor.GetDepthImageHeight(); ++y) {
             for (unsigned int x = 0; x < sensor.GetDepthImageWidth(); ++x) {
                 unsigned int idx = y * sensor.GetDepthImageWidth() + x;
@@ -191,11 +209,13 @@ int main() {
                     vertices[idx].position = Vector4f(MINF, MINF, MINF, MINF);
                     vertices[idx].color = Vector4uc(0, 0, 0, 0);
                 } else {
-                    Vector4f p = Vector4f(depth * (x - cX) / fX, depth * (y - cY) / fY, depth, 1.0f);
+                    Vector4f p = Vector4f(depth * (x - cX) / fX,
+                                          depth * (y - cY) / fY, depth, 1.0f);
                     Vector4f p_world = trajectoryInv * depthExtrinsicsInv * p;
                     vertices[idx].position = p_world;
-                    vertices[idx].color = Vector4uc(colorMap[idx * 4], colorMap[idx * 4 + 1], colorMap[idx * 4 + 2],
-                                                    colorMap[idx * 4 + 3]);
+                    vertices[idx].color =
+                        Vector4uc(colorMap[idx * 4], colorMap[idx * 4 + 1],
+                                  colorMap[idx * 4 + 2], colorMap[idx * 4 + 3]);
                 }
             }
         }
@@ -203,7 +223,8 @@ int main() {
         // write mesh file
         std::stringstream ss;
         ss << filenameBaseOut << sensor.GetCurrentFrameCnt() << ".off";
-        if (!WriteMesh(vertices, sensor.GetDepthImageWidth(), sensor.GetDepthImageHeight(), ss.str())) {
+        if (!WriteMesh(vertices, sensor.GetDepthImageWidth(),
+                       sensor.GetDepthImageHeight(), ss.str())) {
             std::cout << "Failed to write mesh!\nCheck file path!" << std::endl;
             return -1;
         }
