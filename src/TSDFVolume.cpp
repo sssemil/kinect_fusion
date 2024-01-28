cc2736d1d43ed96bfb07602c0260779c78070f03
Emil <emil@esnx.xyz>
2024-01-28 20:37:49 +0100

    Format

--- src/TSDFVolume.cpp
+++ src/TSDFVolume.cpp
@@ -17,7 +17,10 @@ TSDFVolume TSDFVolume::buildSphere() {
     for (int x = 0; x < tsdf.width; x++) {
         for (int y = 0; y < tsdf.height; y++) {
             for (int z = 0; z < tsdf.depth; z++) {
-                tsdf.getVoxel(x, y, z).distance = pow(x - tsdf.width / 2.f, 2) + pow(y - tsdf.height / 2.f, 2) + pow(z - tsdf.depth / 2.f, 2) - pow(radius, 2);
+                tsdf.getVoxel(x, y, z).distance =
+                    pow(x - tsdf.width / 2.f, 2) +
+                    pow(y - tsdf.height / 2.f, 2) +
+                    pow(z - tsdf.depth / 2.f, 2) - pow(radius, 2);
             }
         }
     }
@@ -49,13 +52,14 @@ void TSDFVolume::integrate(const PointCloud& pointCloud,
         const Eigen::Vector3f& normal = normals[i];
 
         // Transform point to TSDF grid coordinates
-        //Eigen::Vector3i voxelCoord = (point / voxelSize).cast<int>();
-        Eigen::Vector3i voxelCoord = ((point + Eigen::Vector3f(2, 2, 2)) / 4.0 * width).cast<int>();
+        // Eigen::Vector3i voxelCoord = (point / voxelSize).cast<int>();
+        Eigen::Vector3i voxelCoord =
+            ((point + Eigen::Vector3f(2, 2, 2)) / 4.0 * width).cast<int>();
 
         // Update voxel if within TSDF volume bounds
-        if (voxelCoord[0] >= 0 && voxelCoord[0] < width
-            && voxelCoord[1] >= 0 && voxelCoord[1] < height
-            && voxelCoord[2] >= 0 && voxelCoord[2] < depth) {
+        if (voxelCoord[0] >= 0 && voxelCoord[0] < width && voxelCoord[1] >= 0 &&
+            voxelCoord[1] < height && voxelCoord[2] >= 0 &&
+            voxelCoord[2] < depth) {
             int index =
                 toLinearIndex(voxelCoord[0], voxelCoord[1], voxelCoord[2]);
             Voxel& voxel = voxels[index];
@@ -81,15 +85,13 @@ void TSDFVolume::storeAsOff(const std::string& filenameBaseOut) {
     std::cout << "TSDFVolume stored as OFF at: " << ss.str() << std::endl;
 
     // convert our TSDF to Volume
-    Volume vol(Vector3d(-0.1,-0.1,-0.1), Vector3d(1.1,1.1,1.1), width, height, depth, 1);
-    for (unsigned int x = 0; x < vol.getDimX(); x++)
-    {
-        for (unsigned int y = 0; y < vol.getDimY(); y++)
-        {
-            for (unsigned int z = 0; z < vol.getDimZ(); z++)
-            {
+    Volume vol(Vector3d(-0.1, -0.1, -0.1), Vector3d(1.1, 1.1, 1.1), width,
+               height, depth, 1);
+    for (unsigned int x = 0; x < vol.getDimX(); x++) {
+        for (unsigned int y = 0; y < vol.getDimY(); y++) {
+            for (unsigned int z = 0; z < vol.getDimZ(); z++) {
                 Voxel vox = getVoxel(x, y, z);
-                vol.set(x,y,z, vox.distance);
+                vol.set(x, y, z, vox.distance);
             }
         }
     }
@@ -101,20 +103,18 @@ void TSDFVolume::storeAsOff(const std::string& filenameBaseOut) {
     // Count duration of marching cubes
     auto start = std::chrono::high_resolution_clock::now();
 
-    for (unsigned int x = 0; x < vol.getDimX() - 1; x++)
-    {
+    for (unsigned int x = 0; x < vol.getDimX() - 1; x++) {
         auto intermittent_end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> elapsed = intermittent_end - start;
         auto it_per_sec = x / elapsed.count();
-        printf("\rMarching Cubes on slice %d of %d (%0.2f seconds) [%0.2f it/s]",
-               x, vol.getDimX(), elapsed.count(), it_per_sec);
+        printf(
+            "\rMarching Cubes on slice %d of %d (%0.2f seconds) [%0.2f it/s]",
+            x, vol.getDimX(), elapsed.count(), it_per_sec);
         fflush(stdout);
 
 #pragma omp parallel for
-        for (unsigned int y = 0; y < vol.getDimY() - 1; y++)
-        {
-            for (unsigned int z = 0; z < vol.getDimZ() - 1; z++)
-            {
+        for (unsigned int y = 0; y < vol.getDimY() - 1; y++) {
+            for (unsigned int z = 0; z < vol.getDimZ() - 1; z++) {
                 ProcessVolumeCell(&vol, x, y, z, 0.00f, &mesh);
             }
         }
@@ -127,8 +127,7 @@ void TSDFVolume::storeAsOff(const std::string& filenameBaseOut) {
 
     // write mesh to file
     std::cout << "Writing mesh to file: " << ss.str() << std::endl;
-    if (!mesh.writeMesh(ss.str()))
-    {
+    if (!mesh.writeMesh(ss.str())) {
         std::cout << "ERROR: unable to write output file!" << std::endl;
     }
 }
